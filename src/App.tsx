1bf99630680a491f2a31f1aaba70648796414d6f
Henrique Machado <henriquesousa@ymail.com>
2025-06-06 15:38:09 -0300

    Listagem, card e deletar passagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/navbar/Navbar'
-import { AuthProvider } from './contexts/AuthContext'
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify'
-import Footer from './components/footer/Footer';
-import { Home } from 'lucide-react';
-import Login from './pages/login/Login';
-import ListPassagens from './components/passagens/listpassagens/ListPassagens';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import { AuthProvider } from "./contexts/AuthContext";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import Footer from "./components/footer/Footer";
+import { Home } from "lucide-react";
+import Login from "./pages/login/Login";
+import ListPassagens from "./components/passagens/listpassagens/ListPassagens";
+import DeletarPassagem from "./components/passagens/deletarpassagem/DeletarPassagem";
 
 function App() {
   return (
@@ -22,13 +23,17 @@ function App() {
               <Route path="/home" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/listpassagens" element={<ListPassagens />} />
+              <Route
+                path="/deletarpassagem/:id"
+                element={<DeletarPassagem />}
+              />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
       </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
